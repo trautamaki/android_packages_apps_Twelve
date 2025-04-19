12701a5d7562caf6291a0a85c9de1f20bbaa723d
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-04-19 02:48:20 +0200

    Twelve: Go back to default renderers factory
    This was killing extensions handling
    
    Change-Id: Ib1993ec6224167fd0a85a6db337e2adebae57d9e

--- app/src/main/java/org/lineageos/twelve/services/TwelveRenderersFactory.kt
+++ app/src/main/java/org/lineageos/twelve/services/TwelveRenderersFactory.kt
@@ -6,53 +6,31 @@
 package org.lineageos.twelve.services
 
 import android.content.Context
-import android.os.Handler
 import androidx.annotation.OptIn
 import androidx.media3.common.util.UnstableApi
-import androidx.media3.exoplayer.Renderer
-import androidx.media3.exoplayer.RenderersFactory
-import androidx.media3.exoplayer.audio.AudioRendererEventListener
+import androidx.media3.exoplayer.DefaultRenderersFactory
 import androidx.media3.exoplayer.audio.DefaultAudioOffloadSupportProvider
 import androidx.media3.exoplayer.audio.DefaultAudioSink
-import androidx.media3.exoplayer.audio.MediaCodecAudioRenderer
-import androidx.media3.exoplayer.mediacodec.DefaultMediaCodecAdapterFactory
-import androidx.media3.exoplayer.mediacodec.MediaCodecSelector
-import androidx.media3.exoplayer.metadata.MetadataOutput
-import androidx.media3.exoplayer.metadata.MetadataRenderer
-import androidx.media3.exoplayer.text.TextOutput
-import androidx.media3.exoplayer.text.TextRenderer
-import androidx.media3.exoplayer.video.VideoRendererEventListener
 
 @OptIn(UnstableApi::class)
 class TwelveRenderersFactory(
-    private val context: Context,
-    private val enableAudioFloatOutput: Boolean,
-) : RenderersFactory {
-    override fun createRenderers(
-        eventHandler: Handler,
-        videoRendererEventListener: VideoRendererEventListener,
-        audioRendererEventListener: AudioRendererEventListener,
-        textRendererOutput: TextOutput,
-        metadataRendererOutput: MetadataOutput
-    ) = arrayOf<Renderer>(
-        MediaCodecAudioRenderer(
-            context,
-            DefaultMediaCodecAdapterFactory(context),
-            MediaCodecSelector.DEFAULT,
-            true,
-            eventHandler,
-            audioRendererEventListener,
-            DefaultAudioSink.Builder(context)
-                .setEnableFloatOutput(enableAudioFloatOutput)
-                .setEnableAudioTrackPlaybackParams(true)
-                .setAudioProcessors(arrayOf(InfoAudioProcessor()))
-                .setAudioTrackBufferSizeProvider(ProxyDefaultAudioTrackBufferSizeProvider)
-                .setAudioOffloadSupportProvider(DefaultAudioOffloadSupportProvider(context))
-                .build()
+    context: Context,
+    enableAudioFloatOutput: Boolean,
+) : DefaultRenderersFactory(context) {
+    init {
+        setEnableAudioFloatOutput(enableAudioFloatOutput)
+        setEnableAudioTrackPlaybackParams(true)
+    }
 
-        ),
-        MetadataRenderer(metadataRendererOutput, eventHandler.looper),
-        MetadataRenderer(metadataRendererOutput, eventHandler.looper),
-        TextRenderer(textRendererOutput, eventHandler.looper),
-    )
+    override fun buildAudioSink(
+        context: Context,
+        enableFloatOutput: Boolean,
+        enableAudioTrackPlaybackParams: Boolean
+    ) = DefaultAudioSink.Builder(context)
+        .setEnableFloatOutput(enableFloatOutput)
+        .setEnableAudioTrackPlaybackParams(enableAudioTrackPlaybackParams)
+        .setAudioProcessors(arrayOf(InfoAudioProcessor()))
+        .setAudioTrackBufferSizeProvider(ProxyDefaultAudioTrackBufferSizeProvider)
+        .setAudioOffloadSupportProvider(DefaultAudioOffloadSupportProvider(context))
+        .build()
 }
