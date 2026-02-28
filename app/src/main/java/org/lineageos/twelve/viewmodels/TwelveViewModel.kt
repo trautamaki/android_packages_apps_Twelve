dc528e215726480bf0e54a04f791f6b01dcc39f4
Timi <timi.rautamaki@gmail.com>
2026-02-28 20:08:53 +0200

    Twelve: Add lastfm integration
    Change-Id: I2addc85db10d3074c64e0d3fb9e445090644629b

--- app/src/main/java/org/lineageos/twelve/viewmodels/TwelveViewModel.kt
+++ app/src/main/java/org/lineageos/twelve/viewmodels/TwelveViewModel.kt
@@ -41,6 +41,7 @@ import org.lineageos.twelve.utils.AudioPreloader
 abstract class TwelveViewModel(application: Application) : AndroidViewModel(application) {
     protected val mediaRepository = getApplication<TwelveApplication>().mediaRepository
     protected val providersRepository = getApplication<TwelveApplication>().providersRepository
+    protected val lastfmRepository = getApplication<TwelveApplication>().lastfmRepository
     protected val outputConfigurationRepository =
         getApplication<TwelveApplication>().outputConfigurationRepository
 
